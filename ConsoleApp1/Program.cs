bf083ca54c6e59dfdd766b89e0c475bf509ddabc
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:33:51 +0200

    colour prikol
    tssss

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a color:");
+            string colour = Console.ReadLine();
+
+          
+            if (colour == "red" || colour == "blue" || colour == "green" || colour == "white")
+            {
+                Console.WriteLine("Your color is " + colour);
+            }
+            else
+            {
+                Console.WriteLine("You don't have this option.");
+            }
+        }
+    }
+}
