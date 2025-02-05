66525b79540af3aa63fda15bb4df1d5d9e0a423c
Opilane TTHK <ihavearm0@gmail.com>
2025-02-05 16:30:19 +0200

    randomNumeros

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RandomNumbers
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+          
+//            Random rnd = new Random();
+//            int number = rnd.Next(1,6);
+//            Console.WriteLine(number);
+          
+
+//            switch(number)
+//                { 
+//            case 1:
+//                    Console.WriteLine("unlucky u got 1");
+//                    break;
+
+//                    case 2:
+//                    Console.WriteLine("u stil unlucky u got 2");
+//                    break;
+//                    case 3:
+//                    Console.WriteLine("nah bro u still unlucky atleast u got 3");
+                    break;
+                    case 4:
+                    Console.WriteLine("u got 4 i have nothing to say");
+                    break;
+                    case 5:
+                    Console.WriteLine("u got 5 it can a be better");
+                    break;
+                    case 6:
+                    Console.WriteLine("6 now ky");
+                            break;
+            }
+        }
+    }
+}
