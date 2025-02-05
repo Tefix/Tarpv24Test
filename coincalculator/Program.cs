193f5d67ce4039775af27f96106a674e99f437d4
Opilane TTHK <opilane@tthk.ee>
2025-02-05 14:52:47 +0200

    CoinCalculator LOL

---
+++ coincalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+
+namespace coincalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            Console.WriteLine(pi);
+
+           
+            double coinDiametr = 25.75;
+            double earthRadius = 6378000000;
+            double aroundearth = 2 * Math.PI * earthRadius;
+            Console.WriteLine("maa umberoot " + aroundearth);
+
+            double result = aroundearth / coinDiametr;
+
+            Console.WriteLine("umber maa mahub nii " + 
+                "palju kah eurosid" + result);
+        }
+    }
+}
