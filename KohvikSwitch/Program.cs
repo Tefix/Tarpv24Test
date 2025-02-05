044dd49c7aa6d3cfb364f8ccd394370c2a21a49a
Opilane TTHK <ihavearm0@gmail.com>
2025-02-05 16:43:14 +0200

    Online cafe

---
+++ KohvikSwitch/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace KohvikSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fantaasia kohvik!");
+
+            Console.WriteLine("\nMenüü (kohvi valik)\n\n");
+            Console.WriteLine("1. Väike tass"); //1.5
+            Console.WriteLine("2. Keskmine tass"); //2.5
+            Console.WriteLine("3. Suur tass"); //3
+
+            Console.WriteLine("\nPalun tee valik numbriga: ");
+            string number = Console.ReadLine();
+
+            switch (number)
+            {
+                case "1":
+                    Console.WriteLine("sa valid  Väike tass");
+                    break;
+                    case "2":
+                    Console.WriteLine("sa valid Keskmine tass");
+                    break;
+                    case "3":
+                    Console.WriteLine(" sa valid Suur tass");
+                    break;
+            }
+        }
+    }
+}
